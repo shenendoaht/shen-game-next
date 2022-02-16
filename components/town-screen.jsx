dbd83deecaa6398dc75eeaf5d2896d6f1c929686
Shenendoah Thompson <47069090+shenendoaht@users.noreply.github.com>
2022-02-16 12:26:31 -0500

    town

--- components/town-screen.jsx
+++ components/town-screen.jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 const TownScreen = () => (
   <section id="town-screen">
     <h1>Lil Ol Town</h1>
-    <Link href="/">house</Link>
-    <Link href="/">arena</Link>
-    <Link href="/">shop</Link>
-    <Link href="/">forest</Link>
+    <Link href="/house">house</Link>
+    <Link href="/arena">arena</Link>
+    <Link href="/shop">shop</Link>
+    <Link href="/forest">forest</Link>
+    <Link href="/tavern">tavern</Link>
   </section>
 );
 export default TownScreen;

2b7068244af19861102b2d01c583e6b80ec72c8e
Shenendoah Thompson <47069090+shenendoaht@users.noreply.github.com>
2022-02-16 12:29:05 -0500

    town

--- components/town-screen.jsx
+++ components/town-screen.jsx
@@ -1,6 +1,6 @@
 import Layout from "./layout";
 import Link from "next/link";
-const TownScreen = () => (
+const TownScreen = () => {
   <section id="town-screen">
     <h1>Lil Ol Town</h1>
     <Link href="/house">house</Link>
@@ -8,6 +8,6 @@ const TownScreen = () => (
     <Link href="/shop">shop</Link>
     <Link href="/forest">forest</Link>
     <Link href="/tavern">tavern</Link>
-  </section>
-);
+  </section>;
+};
 export default TownScreen;

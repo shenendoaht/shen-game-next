68a47e25ed295bdc69dbff3711c367c6d7f0233c
Shenendoah Thompson <47069090+shenendoaht@users.noreply.github.com>
2022-02-17 09:06:04 -0500

    wel screen

--- components/welcome-screen.jsx
+++ components/welcome-screen.jsx
@@ -22,6 +22,32 @@ const WelcomeScreen = () => {
 
   return (
     <section id="welcome-screen">
+      <header>
+        <h1>Welcome</h1>
+      </header>
+      <div id="scroll">
+        <div id="scroll-text">
+          <h2>Lil Quest</h2>
+          <p>
+            This game is <strong> still </strong> a work in progress. <br />
+            It demonstrates my skills with HTML, CSS and JavaScript. <br />
+            <em>Bon chance</em> on this lil adventure!
+          </p>
+          <p id="instructions">
+            Name your character, explore the world and defeat your adversaries!
+          </p>
+          <p id="special-thx">
+            Special thanks: <br />
+            frenz <br />
+            romanz <br />
+            countrymen? <br />
+            Squirrelly <br />
+            <em>*mini tribal* </em>
+          </p>
+        </div>
+        {/* <!-- scroll text --> */}
+      </div>
+      {/* <!-- scroll  --> */}
       <p>Your name is {!!name ? "not " : ""}empty.</p>
       {!!name ? (
         <form onSubmit={deleteName}>
@@ -55,3 +81,40 @@ export default WelcomeScreen;
 //   );
 // };
 // export default Header;
+{
+  /* 
+      <!-- scroll -->
+      <div id="character">
+        <input
+          class="input"
+          type="text"
+          id="input-el"
+          placeholder="Name your Character"
+          required
+        />
+        <button class="btn" id="player-btn">Name Character</button>
+        <div id="roll-btns">
+          <button class="btn roll-btn" id="hp-btn">Roll HP</button>
+          <button class="btn roll-btn" id="str-btn">Roll STR</button>
+          <button class="btn roll-btn" id="def-btn">Roll DEF</button>
+          <button class="btn roll-btn" id="ab-btn">Roll AB</button>
+          <button class="btn roll-btn" id="gp-btn">Roll GP</button>
+        </div>
+        <!-- roll btns-->
+      </div>
+      <!-- character-->
+      <div id="wel-player">
+        <div class="player"></div>
+        <div class="stats" id="wel-stats"></div>
+      </div>
+
+      <footer>
+        <nav>
+          <ul>
+            <li><button class="btn nav-btn" id="town-btn">To Town</button></li>
+          </ul>
+        </nav>
+        <h5>&copy ShenendoahT 2022</h5>
+      </footer>
+    </section> */
+}

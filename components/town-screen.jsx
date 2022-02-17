d1274eb30ddadb5097945f0a8a666b08dd3a6fda
Shenendoah Thompson <47069090+shenendoaht@users.noreply.github.com>
2022-02-17 09:18:40 -0500

    town

--- components/town-screen.jsx
+++ components/town-screen.jsx
@@ -1,13 +1,56 @@
-import Layout from "./layout";
 import Link from "next/link";
 const TownScreen = () => {
-  <section id="town-screen">
-    <h1>Lil Ol Town</h1>
-    <Link href="/house">house</Link>
-    <Link href="/arena">arena</Link>
-    <Link href="/shop">shop</Link>
-    <Link href="/forest">forest</Link>
-    <Link href="/tavern">tavern</Link>
+  <section id="townSquare">
+    <header>
+      <h1>Town</h1>
+      <h2 id="town-message"></h2>
+      <h4 id="town-dialogue"></h4>
+    </header>
+    <button id="explore-town">Explore</button>
+    <div id="town-content">
+      <div id="fountain">
+        <button id="fountain-btn">Search Fountain</button>
+        <div id="coins"></div>
+        <button id="coin-btn">Take coins</button>
+      </div>
+      <div id="people">
+        <div id="monk">
+          <button id="monk-btn">Talk to Monk</button>
+        </div>
+        <div id="oldwoman">
+          <button id="maid-btn">Talk to Old Woman</button>
+        </div>
+      </div>
+      {/* <!-- people --> */}
+    </div>
+    {/* <div id="town-player">
+        <div class="player"></div>
+        <div class="stats" id="town-stats"></div>
+      </div> */}
+    <Link href="/forest"> forest </Link>
+    <Link href="/tavern"> tavern </Link>
+    <Link href="/house"> house </Link>
+    <Link href="/arena"> arena </Link>
+    <Link href="/shop"> shop </Link>
   </section>;
 };
 export default TownScreen;
+//  <nav>
+//    <ul>
+//      <li>
+//        <button id="house-btn">House</button>
+//      </li>
+//      <li>
+//        <button id="tav-btn">Tavern</button>
+//      </li>
+//      <li>
+//        <button id="shop-btn">Shop</button>
+//      </li>
+//      <li>
+//        <button id="arena-btn">Arena</button>
+//      </li>
+//      <li>
+//        <button id="forest-btn">Forest</button>
+//      </li>
+//    </ul>
+//  </nav>;

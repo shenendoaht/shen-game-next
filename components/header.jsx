0361c918c55a265d828f038f6c4db089744c385b
Shenendoah Thompson <47069090+shenendoaht@users.noreply.github.com>
2022-02-26 11:54:01 -0500

    2022-26-02 header display flex

--- components/header.jsx
+++ components/header.jsx
@@ -7,12 +7,14 @@ const Header = () => {
   return (
     <header>
       <h1>Welcome, {name}!</h1>
-      <p>Level: {level ?? "0"}</p>
-      <p> Gold: {gold ?? "0"}</p>
-      <p> HP: {HP ?? "0"} </p>
-      <p> STR: {STR ?? "0"}</p>
-      <p> DEF: {DEF ?? "0"}</p>
-      <p> AB: {AB ?? "0"}</p>
+      <div id="stat-disp">
+        <p>Level: {level ?? "0"}</p>
+        <p> Gold: {gold ?? "0"}</p>
+        <p> HP: {HP ?? "0"} </p>
+        <p> STR: {STR ?? "0"}</p>
+        <p> DEF: {DEF ?? "0"}</p>
+        <p> AB: {AB ?? "0"}</p>
+      </div>
       <hr />
     </header>
   );
